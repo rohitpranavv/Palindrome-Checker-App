f424cbe3f960b5e413834202790f1310ea47da61
UDAYB400 <udaykumar33322@gmail.com>
2026-02-17 15:50:07 +0530

    UC2:Hardcoded palindrome

--- src/UseCase1PalindromeCheckerApp.java
+++
@@ -1,9 +0,0 @@
-public class UseCase1PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Management System  ");
-
-        System.out.println("Application Version: 1.0");
-
-        System.out.println("System intialized sucessfully");
-    }
-}
