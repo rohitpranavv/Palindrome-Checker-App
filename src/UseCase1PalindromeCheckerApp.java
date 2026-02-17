aca9e3b860f1b5566ab61b9fe3369cfae58b5899
UDAYB400 <udaykumar33322@gmail.com>
2026-02-17 15:26:39 +0530

    UC1

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class UseCase1PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System  ");
+
+        System.out.println("Application Version: 1.0");
+
+        System.out.println("System intialized sucessfully");
+    }
+}
