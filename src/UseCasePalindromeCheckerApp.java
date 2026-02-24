27b1b600f0d2a25052e2e89f5cae025ec212988f
UDAYB400 <udaykumar33322@gmail.com>
2026-02-24 15:03:24 +0530

    UC4:Character Array Based Palindrome Check

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
+
         Scanner S = new Scanner(System.in);
         System.out.print("enter the String");
         String input = S.nextLine();
@@ -11,16 +12,21 @@ public class UseCasePalindromeCheckerApp {
         boolean isPalindrome=true;
 
         while(start<end){
+
            if (chars[start] != chars[end]){
                isPalindrome=false;
                break;
             }
+
            start++;
            end--;
         }
+
+
         if(isPalindrome){
             System.out.println("Is Palindrome?: True");
         }
+
         else{
             System.out.println("Is Palindrome?: False");
         }
